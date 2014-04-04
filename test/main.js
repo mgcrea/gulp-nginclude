d956ce436e020c057cfaa7f5756b27a92d8eaac5
Olivier Louvignes <olivier@mg-crea.com>
2014-04-04 13:32:43 +0200

    test(main): add unit tests

---
+++ test/main.js
@@ -0,0 +1,88 @@
+var pkg = require('../package.json');
+var nginclude = require('../' + pkg.main);
+var path = require('path');
+var extend = require('lodash.assign');
+var File = require('gulp-util').File;
+var Buffer = require('buffer').Buffer;
+var should = require('should');
+require('mocha');
+
+describe('gulp-nginclude', function() {
+
+  var defaults = {
+    path: path.join(__dirname, 'fixtures/file.html'),
+    cwd: __dirname,
+    base: path.join(__dirname, 'fixtures')
+  };
+  console.log(defaults);
+
+  beforeEach(function() {
+  });
+
+  describe('nginclude()', function() {
+
+    it('should pass through file', function(done) {
+
+      var fixture = new File(extend({contents: new Buffer('<div>foo</div>')}, defaults));
+
+      var stream = nginclude();
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+        should.equal(newFile.contents.toString(), '<div>foo</div>');
+        newFile.path.should.equal(path.join(__dirname, 'fixtures/file.html'));
+        newFile.relative.should.equal('file.html');
+      });
+      stream.once('end', done);
+      stream.write(fixture);
+      stream.end();
+
+    });
+
+    it('should properly include the target file', function(done) {
+
+      var fixture = new File(extend({contents: new Buffer('<div>foo</div>\n<div ng-include="\'bar.html\'"></div>\n<div>baz</div>')}, defaults));
+      var include = '<!-- ngInclude: \'bar.html\' -->\n<div>bar</div>';
+
+      var stream = nginclude();
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+        should.equal(newFile.contents.toString(), ['<div>foo</div>', include, '<div>baz</div>'].join('\n'));
+        newFile.path.should.equal(path.join(__dirname, 'fixtures/file.html'));
+        newFile.relative.should.equal('file.html');
+      });
+      stream.once('end', done);
+      stream.write(fixture);
+      stream.end();
+
+    });
+
+    it('should properly handle dynamic ngIncludes', function(done) {
+
+      var fixture = new File(extend({contents: new Buffer('<div ng-include="foo"></div>\n<div ng-include="\'bar.html\'"></div>\n<div>baz</div>')}, defaults));
+      var include = '<!-- ngInclude: \'bar.html\' -->\n<div>bar</div>';
+
+      var stream = nginclude();
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+        should.equal(newFile.contents.toString(), ['<div ng-include="foo"></div>', include, '<div>baz</div>'].join('\n'));
+        newFile.path.should.equal(path.join(__dirname, 'fixtures/file.html'));
+        newFile.relative.should.equal('file.html');
+      });
+      stream.once('end', done);
+      stream.write(fixture);
+      stream.end();
+
+    });
+
+  });
+
+});
